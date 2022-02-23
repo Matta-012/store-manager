1c46e292c87f54127b9e2f220e8058852f3c8596
Joao Marco <joaomarco.matta@gmail.com>
2022-02-23 15:31:03 -0300

    chore: add db config at .env file and add express async errors

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 require('dotenv').config();
 
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
