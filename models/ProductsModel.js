5d4325ae8049af229996ee5b12989f06317eef09
Joao Marco <joaomarco.matta@gmail.com>
2022-02-24 17:26:03 -0300

    chore: add order by on queries

--- models/ProductsModel.js
+++ models/ProductsModel.js
@@ -1,14 +1,14 @@
 const connection = require('./connection');
 
 const getAll = async () => {
-  const query = 'SELECT id, name, quantity FROM products ORDER BY id;';
+  const query = 'SELECT id, name, quantity FROM StoreManager.products ORDER BY id;';
   const [result] = await connection.execute(query);
 
   return result;
 };
 
 const getById = async (id) => {
-  const query = 'SELECT id, name, quantity FROM products WHERE id = ? ORDER BY id;';
+  const query = 'SELECT id, name, quantity FROM StoreManager.products WHERE id = ? ORDER BY id;';
   const [result] = await connection.execute(query, [id]);
 
   return result;
