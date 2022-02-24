0a6365b91494e7c41fd80f974192352f57169ccd
Joao Marco <joaomarco.matta@gmail.com>
2022-02-24 16:16:19 -0300

    test: refactor ProductsController.getById method tests

--- test/unit/controllers/ProductsController.test.js
+++ test/unit/controllers/ProductsController.test.js
@@ -16,55 +16,85 @@ describe('Products Controller tests', () => {
   });
 
   describe('get all Products', () => {
-    const getAllResponse = {
-      code: 200,
-      data: [
-        {
-          "id": 1,
-          "name": "produto A",
-          "quantity": 10
-        },
-        {
-          "id": 2,
-          "name": "produto B",
-          "quantity": 20
-        }
-      ],
-    };
+    describe('when all products are found', () => {
+      const getAllResponse = {
+        code: 200,
+        data: [
+          {
+            "id": 1,
+            "name": "produto A",
+            "quantity": 10
+          },
+          {
+            "id": 2,
+            "name": "produto B",
+            "quantity": 20
+          }
+        ],
+      };
 
-    before(() => {
-      sinon.stub(ProductsService, 'getAll').resolves(getAllResponse);
+      before(() => {
+        sinon.stub(ProductsService, 'getAll').resolves(getAllResponse);
+      });
+
+      after(() => {
+        ProductsService.getAll.restore();
+      });
+
+      it('Should respond with status code "200"', async () => {
+        await ProductsController.getAll(request, response, next);
+
+        expect(response.status.calledWith(getAllResponse.code)).to.be.true;
+      });
+
+      it('Response json should return expected data from database', async () => {
+        await ProductsController.getAll(request, response, next);
+
+        expect(response.json.calledWith(getAllResponse.data)).to.be.true;
+      });
     });
 
-    after(() => {
-      ProductsService.getAll.restore();
-    });
+    describe('when all products are not found', () => {
+      const getAllResponse = {
+        code: 404,
+        message: 'Products not found',
+      };
 
-    it('Should respond with status code 200', async () => {
-      await ProductsController.getAll(request, response, next);
+      before(() => {
+        sinon.stub(ProductsService, 'getAll').resolves(getAllResponse);
+      });
 
-      expect(response.status.calledWith(200)).to.be.true;
-    });
+      after(() => {
+        ProductsService.getAll.restore();
+      });
 
-    it('Response json should return expected data from database', async () => {
-      await ProductsController.getAll(request, response, next);
+      it('Should respond with status code "404"', async () => {
+        await ProductsController.getAll(request, response, next);
 
-      expect(response.json.calledWith(getAllResponse.data)).to.be.true;
+        expect(response.status.calledWith(getAllResponse.code)).to.be.true;
+      });
+
+      it('Response json should return error message "Products not found"', async () => {
+        await ProductsController.getAll(request, response, next);
+
+        expect(response.json.calledWith(getAllResponse.message)).to.be.true;
+      });
     });
   });
 
   describe('get Product by ID', () => {
     describe('When response returns the Product', () => {
       const id = 1;
-      const getByIdResponse = [
-        [
+      const getByIdResponse = {
+        code: 200,
+        data: [
           {
             "id": 1,
             "name": "produto A",
             "quantity": 10
           },
-        ]
-      ];
+        ],
+      };
 
       before(() => {
         request.params = { id };
@@ -75,16 +105,45 @@ describe('Products Controller tests', () => {
         ProductsService.getById.restore();
       });
 
-      it('Should respond with status code 200', async () => {
+      it('Should respond with status code "200"', async () => {
         await ProductsController.getById(request, response, next);
 
-        expect(response.status.calledWith(200)).to.be.true;
+        expect(response.status.calledWith(getByIdResponse.code)).to.be.true;
       });
 
       it('Response json should return expected data from database', async () => {
         await ProductsController.getAll(request, response, next);
 
-        expect(response.json.calledWith(getAllResponse)).to.be.true;
+        expect(response.json.calledWith(getByIdResponse.data)).to.be.true;
+      });
+    });
+
+    describe('When the product is not found', () => {
+      const id = 6;
+      const getByIdResponse = {
+        code: 200,
+        message: 'Product not found',
+      };
+
+      before(() => {
+        request.params = { id };
+        sinon.stub(ProductsService, 'getById').resolves(getByIdResponse);
+      });
+
+      after(() => {
+        ProductsService.getById.restore();
+      });
+
+      it('Should respond with status code "404"', async () => {
+        await ProductsController.getAll(request, response, next);
+
+        expect(response.status.calledWith(getByIdResponse.code)).to.be.true;
+      });
+
+      it('Response json should return error message "Product not found"', async () => {
+        await ProductsController.getAll(request, response, next);
+
+        expect(response.json.calledWith(getByIdResponse.message)).to.be.true;
       });
     });
   });
