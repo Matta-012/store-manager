a024f9291d73a34471d1930509fc1e5895c79abe
Joao Marco <joaomarco.matta@gmail.com>
2022-02-24 11:16:04 -0300

    test: add tests for ProductsModel.getById

--- test/unit/models/ProductsModel.test.js
+++ test/unit/models/ProductsModel.test.js
@@ -29,25 +29,93 @@ describe('Products Model tests', () => {
       connection.execute.restore();
     });
 
-    it('Returns an array not empty', async () => {
+    it('Should return an array not empty', async () => {
       const getAllProducts = await ProductsModel.getAll();
 
       expect(getAllProducts).to.be.an('array');
       expect(getAllProducts).to.be.not.empty;
     });
 
-    it('Returns an array of objects', async () => {
+    it('Should return an array of objects', async () => {
       const getAll = await ProductsModel.getAll();
 
       expect(getAll[0]).to.be.an('object');
       expect(getAll[1]).to.be.an('object');
     });
 
-    it('Objects have the following properties: "id", "name", "quantity"', async () => {
+    it('Objects should have the following properties: "id", "name", "quantity"', async () => {
       const getAll = await ProductsModel.getAll();
 
       expect(getAll[0]).to.include.all.keys('id', 'name', 'quantity');
       expect(getAll[1]).to.include.all.keys('id', 'name', 'quantity');
+    });
+  });
+
+  describe('get Product by ID', () => {
+    describe('When the product is found', () => {
+      const id = 1;
+      const getByIdResponse = [
+        [
+          {
+            "id": 1,
+            "name": "produto A",
+            "quantity": 10
+          },
+        ]
+      ];
+
+      before(() => {
+        sinon.stub(connection, 'execute').resolves(getByIdResponse);
+      });
+
+      after(() => {
+        connection.execute.restore();
+      });
+
+      it('Should return an array with one product', async () => {
+        const product = await ProductsModel.getById(id);
+  
+        expect(product).to.be.an('array');
+        expect(product).to.be.not.empty;
+        expect(product).to.be.have.length(1);
+      });
+  
+      it('Should return an array with an object', async () => {
+        const product = await ProductsModel.getById(id);
+  
+        expect(product[0]).to.be.an('object');
+      });
+  
+      it('Product object should have the following properties: "id", "name", "quantity"', async () => {
+        const product = await ProductsModel.getById(id);
+  
+        expect(product[0]).to.include.all.keys('id', 'name', 'quantity');
+      });
+  
+      it('Product object returned should be deep equal the product searched', async () => {
+        const product = await ProductsModel.getById(id);
+
+        expect(product).to.be.deep.equal(getByIdResponse[0]);
+      });
+    });
+
+    describe('When the product is not found', () => {
+      const id = 5;
+      const getByIdResponse = [[]];
+  
+      before(() => {
+        sinon.stub(connection, 'execute').resolves(getByIdResponse);
+      });
+  
+      after(() => {
+        connection.execute.restore();
+      });
+
+      it('Should return "null"', async () => {
+        const product = await ProductsModel.getById(id);
+
+        expect(product).to.be.equal(null);
+      });
     });
   });
 });
