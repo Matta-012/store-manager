d8cfdc1591763820b20cd999706375e3851e3d7f
Joao Marco <joaomarco.matta@gmail.com>
2022-02-24 11:16:39 -0300

    feat: add getById method on ProductsModel

--- models/ProductsModel.js
+++ models/ProductsModel.js
@@ -1,12 +1,20 @@
 const connection = require('./connection');
 
 const getAll = async () => {
-  const query = 'SELECT id, name, quantity FROM products';
+  const query = 'SELECT id, name, quantity FROM products;';
   const [result] = await connection.execute(query);
 
   return result;
 };
 
+const getById = async (id) => {
+  const query = 'SELECT id, name, quantity FROM products WHERE id = ?;';
+  const [result] = await connection.execute(query, [id]);
+
+  return result.length === 0 ? null : result;
+};
+
 module.exports = {
   getAll,
+  getById,
 };
